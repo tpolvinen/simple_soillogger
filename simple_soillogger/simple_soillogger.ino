d5836a9ee46ea827504291ef31d340342fad0afb
Tatu P <tatu.polvinen@helsinki.fi>
2020-05-14 12:39:54 +0300

    Add SDISerial.h library

--- simple_soillogger/simple_soillogger.ino
+++ simple_soillogger/simple_soillogger.ino
@@ -1,6 +1,17 @@
 #include <SPI.h>
 #include <Controllino.h>
 
+//------------------------------------------------------------------------------
+#include <SDISerial.h>
+
+#define DATALINE_PIN CONTROLLINO_IN0  // pin 18 on Arduino Mega //choose a pin that supports interupts
+#define INVERTED 1
+
+SDISerial sdi_serial_connection(DATALINE_PIN, INVERTED);
+
+//------------------------------------------------------------------------------
+
+
 void setup() {
   // put your setup code here, to run once:
 
