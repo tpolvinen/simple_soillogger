7701c421a0fda708a42686a45b7d8d5b4a4119e5
Tatu P <tatu.polvinen@helsinki.fi>
2020-05-14 14:48:22 +0300

    Change response wait time ms numbers to variable, slight refactoring

--- simple_soillogger/simple_soillogger.ino
+++ simple_soillogger/simple_soillogger.ino
@@ -13,6 +13,7 @@ SDISerial sdi_serial_connection(DATALINE_PIN, INVERTED);
 void setup() {
   sdi_serial_connection.begin();
   Serial.begin(9600);
+  delay(500);
   Serial.println("OK INITIALIZED");
   delay(3000);
   // startup delay to allow sensor to powerup
@@ -20,20 +21,20 @@ void setup() {
 }
 
 void loop() {
-  uint8_t wait_for_response_ms = 1000;
-  char* response = get_measurement();
+  const int16_t responseWaitMs = 1000;
+  char* response = get_measurement(responseWaitMs);
   Serial.println(response!=NULL&&response[0] != '\0'?response:"No Response!");
   delay(500);
 }
 
 //------------------------------------------------------------------------------
 
-char* get_measurement() {
-  char* service_request = sdi_serial_connection.sdi_query("?M!10013", 1000);
+char* get_measurement(int16_t waitMs) {
+  char* service_request = sdi_serial_connection.sdi_query("?M!10013", waitMs);
   // the time  above is to wait for service_request_complete
-  char* service_request_complete = sdi_serial_connection.wait_for_response(1000);
+  char* service_request_complete = sdi_serial_connection.wait_for_response(waitMs);
   // will return once it gets a response
-  return sdi_serial_connection.sdi_query("?D0!", 1000);
+  return sdi_serial_connection.sdi_query("?D0!", waitMs);
 }
 
 //------------------------------------------------------------------------------
